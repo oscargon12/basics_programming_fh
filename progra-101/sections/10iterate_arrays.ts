16bfe3d470eb96a2413e8f374893c62e89582311
oscargon12 <oscargon12@gmail.com>
2023-02-19 00:30:31 -0500

    functions & arrays

---
+++ progra-101/sections/10iterate_arrays.ts
@@ -0,0 +1,11 @@
+let people: string[] = ['Hugo', 'Paco', 'Luis'];
+let salaries: number[] = [1000, 1200, 1500]
+//let nums: number[] = [1,2,3,4,5];
+
+if (people[3]) {console.log(people[3])} //Si hay algo en esa posicion, imprímalo
+
+for(let i = 0; i < people.length; i++){
+    let person = people[i];
+    let salary = salaries[i]
+    console.log(`El empleado ${person} tiene un sueldo de ${salary}`)
+}
---
+++ progra-101/sections/9import_function.ts
@@ -0,0 +1,6 @@
+import { numsTimesTable } from "../helpers/print-mult-tables"
+
+numsTimesTable(5)
+numsTimesTable(2, 11)
+numsTimesTable(4)
+numsTimesTable(9, -9)
