ceb1e44db69d8d3890f1c2d4840afb41b4d0bc40
oscargon12 <oscargon12@gmail.com>
2023-03-15 23:47:50 -0500

    game images

--- hanging/src/App.tsx
+++ hanging/src/App.tsx
@@ -1,10 +1,27 @@
 import './App.css';
+import { HangImage } from './components/HangImage';
+import {letters} from './helpers/letters';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Hola Mundo</h1>
+      {/* Images */}
+      <HangImage />
+      
+      {/* hidden word */}
+      <h3>_ _ _ _ _ _ _ _ _ _ </h3>
+      
+      {/* tries counter */}
+      <h3>Intentos: 0</h3>
+      
+      {/* buttons */}
+      {
+        letters.map( (letter) => (
+          <button key={ letter }>{ letter }</button>
+        ))
+      }
+      
     </div>
   )
 };
---
+++ hanging/src/components/HangImage.tsx
@@ -0,0 +1,8 @@
+import image0 from '../assets/0.png';
+
+export function HangImage() {
+    
+    return (
+        <img src={image0} className="hang-image" alt="Hang Image" />
+    ); // JSX element
+}
