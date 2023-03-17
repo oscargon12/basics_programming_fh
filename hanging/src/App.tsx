8d76c3992e94df54f6884bb971bd9f9f0660073e
oscargon12 <oscargon12@gmail.com>
2023-03-17 17:40:30 -0500

    hangman game logic

--- hanging/src/App.tsx
+++ hanging/src/App.tsx
@@ -1,24 +1,67 @@
 import './App.css';
+import { useState } from 'react';
 import { HangImage } from './components/HangImage';
 import {letters} from './helpers/letters';
 
 function App() {
 
+  // const word: string = 'COMPUTADORA'
+  const [word] = useState('COMPUTADORA');
+  // Si quiero permitir que la palabra se actualice cambio el formato de vaiable a useState
+  const [hidderWord, setHiddenWord] = useState('_ '.repeat(word.length)); //setHiddenWord es la funcion para actualizar la palabra
+  //el setHiddenWord cambia el estado y las va sumando las letras al array hiddenWordArray
+  // El .repeat(word.lengt) me dirá cuantos caracteres tiene la palabra
+  const [attemps, setAttemps] = useState(0);
+
+  const checkLetter = (letter: string) => {
+    //console.log(letter)
+
+    //Existe la letra dentro de la palabra?
+    if(!word.includes(letter)){
+      console.log(letter + ' No existe')
+      setAttemps( Math.min( attemps + 1, 9) ); // Math.min mantiene el valor minimo entre 0 y 9
+      return;
+    }
+
+    //reemplazando el guion por la letra correcta
+    const hiddenWordArray: string[] = hidderWord.split(' '); //separa el hiddenWord en lineas separadas
+    console.log(hiddenWordArray);
+
+    for(let i = 0; i < word.length; i++){
+      
+      //Si la letra de esa posición es igual a letter...
+      if(word[i] === letter){
+        hiddenWordArray[i] = letter; //cambie el guón por la letra correcta
+      }
+    }
+
+    console.log(hiddenWordArray);
+
+    setHiddenWord( hiddenWordArray.join(' ') ); //Une los items del array que esten separados por un espacio vacío
+    //esta funcion viene del useState
+  }
+
   return (
     <div className="App">
       {/* Images */}
-      <HangImage />
+      <HangImage imageNumber={attemps}/> 
+      {/* imageNumber es una prop que se conecta con: 🔹 */}
       
       {/* hidden word */}
-      <h3>_ _ _ _ _ _ _ _ _ _ </h3>
+      <h3>{ hidderWord }</h3>
+      {/* Este hiddenWord se actualiza con el useState */}
       
       {/* tries counter */}
-      <h3>Intentos: 0</h3>
+      <h3>Intentos: {attemps}</h3>
       
       {/* buttons */}
       {
         letters.map( (letter) => (
-          <button key={ letter }>{ letter }</button>
+          <button 
+          key={ letter }
+          onClick={ () => checkLetter(letter) }>
+            { letter }
+          </button>
         ))
       }
       
--- hanging/src/components/HangImage.tsx
+++ hanging/src/components/HangImage.tsx
@@ -1,8 +1,41 @@
 import image0 from '../assets/0.png';
+import image1 from '../assets/1.png';
+import image2 from '../assets/2.png';
+import image3 from '../assets/3.png';
+import image4 from '../assets/4.png';
+import image5 from '../assets/5.png';
+import image6 from '../assets/6.png';
+import image7 from '../assets/7.png';
+import image8 from '../assets/8.png';
+import image9 from '../assets/9.png';
 
-export function HangImage() {
+const images: string[] = [
+    image0,
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+];
+
+interface Props { 
+    imageNumber: number;
+}
+{/* 🔹 Estas props se reciben desde el componente App.tsx*/}
+
+export function HangImage( {imageNumber}: Props ) {
     
+    console.log({imageNumber}.imageNumber)
+    
+    if(imageNumber >= 9){
+        imageNumber = 9;
+    }
+
     return (
-        <img src={image0} className="hang-image" alt="Hang Image" />
+        <img src={images[imageNumber]} className="hang-image" alt="Hang Image" />
     ); // JSX element
 }
