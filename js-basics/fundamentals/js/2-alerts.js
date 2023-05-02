d1dad8affa79b0ba64fb0b4db934b9dd01e62e6c
oscargon12 <oscargon12@gmail.com>
2023-05-02 00:22:13 -0500

    alerts

--- js-basics/fundamentals/js/2-alerts.js
+++ js-basics/fundamentals/js/2-alerts.js
@@ -6,3 +6,5 @@ const selection = confirm('Está seguro de borrar esto?')
 console.log(selection);
 
 // alert, prompt y confirm están dentro del objeto window
+// El objeto window no se puede ejecutar en node (terminal)
+// En node se usa el objeto global: console.log( global )
