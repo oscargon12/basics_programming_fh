b104c288427419cef5d4f6437568974383bf6e22
oscargon12 <oscargon12@gmail.com>
2023-05-03 00:31:18 -0500

    primitive arrays

---
+++ js-basics/fundamentals/js/3-primitivos.js
@@ -0,0 +1,5 @@
+let nombre = 'Peter Parker'; //la comilla sencilla es más ligera que la doble
+console.log({nombre});
+
+nombre = "Ben Parker";
+console.log({nombre});
---
+++ js-basics/fundamentals/js/4-arreglos.js
@@ -0,0 +1,12 @@
+let arreglo = [
+    true,
+    123,
+    'Oscar',
+    1 + 2,
+    function (){},
+    () => {},
+    {a :1},
+    ['X', 'Megaman', 'Zero', 'Dr light'],
+]
+
+console.log( arreglo[7][3] );
