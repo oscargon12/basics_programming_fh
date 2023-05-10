cbe262a46a77329269f973530ad09a497ae8fc3e
oscargon12 <oscargon12@gmail.com>
2023-05-10 00:10:26 -0500

    boolean logic

--- js-basics/fundamentals/js/8-if-else.js
+++ js-basics/fundamentals/js/8-if-else.js
@@ -6,3 +6,31 @@ if( day === 0 ){
 } else if (día === 1){
     console.log('Lunes');
 }
+
+
+// *** Logica booleana ***
+//Puedo negar lo que retorne la función
+const returnTrue = () => {
+    return true;
+}
+
+console.log( returnTrue() );
+console.log( !returnTrue() );
+
+
+//** Or & And ** */
+console.log(  true && true ); //true debe cumplir todo
+
+console.log(true || true); //true se debe cumplir lmenos uno
+
+// Otros usos del OR y AND
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalse = false;
+
+//Toma el último valor
+const a1 = false && 'Hola Mundo' && 150; // false
+const a2 = 'Hola' && 'Mundo' && 'soy falso' && true; //true
+const a3 = soyFalse || 'ya no soy falso'; //ya no soy falso
+const a4 = soyFalse || soyUndefined || soyNull || 'Ya no soy falso de nuevo' || true; //
+const a5 = soyFalse || soyUndefined || returnTrue() || 'Ya no soy falso de nuevo' || true; //true, por la funcion returnTrue()
---
+++ js-basics/fundamentals/js/9-workshop1.js
@@ -0,0 +1,20 @@
+const daysArr = [
+    'sunday',
+    'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+    'Saturday'
+];
+
+console.log(daysArr[0]);
+
+const daysObj = {
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday'
+};
+
+console.log(daysArr[1]);
+
