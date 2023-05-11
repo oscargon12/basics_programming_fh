bfa69af0273e45abb1bc8f43433d49d0c335ff8b
oscargon12 <oscargon12@gmail.com>
2023-05-11 00:17:09 -0500

    ternary 1

---
+++ js-basics/fundamentals/js/10-ternario.js
@@ -0,0 +1,32 @@
+//restaurante: entre semana abre a las 11
+//fines de semana a las 9
+
+const day = 0;
+const currentHour = 10;
+
+let openHour;
+let message;  //está abierto o cerrado
+
+/* if ( day === 0 || day === 6 ){
+    console.log('Finde');
+    openHour = 9
+} else {
+    console.log('Entre semana');
+    openHour = 11
+} */
+//👆 Se cambia todo eso por esto:
+
+openHour = ( [0,6].includes( day ) ) ? 9 : 11;
+//El includes dice si en ese array está el día, entonces...
+
+
+/* if( currentHour >= openHour ){
+    message = 'Esta abierto'
+} else {
+    message = `Esta cerrado, hoy abrimos a las ${ openHour }`
+} */
+//👆 Se cambia todo eso por esto:
+
+message = (currentHour >= openHour) ? 'Esta abierto' : `Esta cerrado, hoy abrimos a las ${ openHour }`;
+
+console.log(message);
