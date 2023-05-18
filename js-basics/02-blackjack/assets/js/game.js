e5fca705865994cd2a32ba304e28097cf5677ffb
oscargon12 <oscargon12@gmail.com>
2023-05-18 00:33:43 -0500

    take card $ card value

--- js-basics/02-blackjack/assets/js/game.js
+++ js-basics/02-blackjack/assets/js/game.js
@@ -19,7 +19,32 @@ const createDeck = () =>{
             deck.push( special + type );
         }
     }
+    
+    deck = _.shuffle(deck);
     console.log( deck );
+    return deck;
 }
 
 createDeck();
+
+//Funcion que permite tomar una carta
+const takeCard = () => {
+
+    /* const takenCard = deck[deck.length -1];
+    deck.pop(); */
+    if(deck.length === 0){
+        throw 'No hay cartas en el deck'
+    }
+
+    const takenCard = deck.pop();
+    console.log(takenCard);
+    return takenCard;
+}
+//takeCard();
+
+const cardValue = (card) => {
+    const value = card.substring(0, card.length - 1);
+    console.log({value})
+}
+
+cardValue('2D');
