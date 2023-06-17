afe500b0ef75399cf191d54cb13e9e50d2d1bc2f
oscargon12 <oscargon12@gmail.com>
2023-06-17 13:39:40 -0500

    jackblack with vite first commit

---
+++ js-basics/01-fundamentals/js/loops/1-while_do-while.js
@@ -0,0 +1,17 @@
+// while
+const carsBrands = ['chevrolet', 'Kia', 'Renault', 'Mazda'];
+
+let i = 0;
+while( carsBrands[i] ){
+  console.log( carsBrands[i] );
+  i++
+}
+
+//DO WHILE Entra al ciclo almenos una vez
+console.log('** Do while **')
+
+let j = 0;
+do{
+  console.log( carsBrands[j] );
+  j++
+} while ( carsBrands[j] )
